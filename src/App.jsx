879cbfe0347fbdd4c0ec035687fa5ef26ebe9887
CodeWithShubham-svg <hypersect000@gmail.com>
2025-04-14 15:04:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,25 @@ import {
   useLocation
 } from 'react-router-dom';
 
-import Header from './components/Header';
+// universal nav bar
 import Navbar from './components/PrivacyNavbar';
-import LocationMap from './components/locationMap';
-import Footer from './components/Footer';
+
+// components of landing page
+import Header from './components/Header';
 import About from './components/About';
-import FloorPlans from './components/FloorPlans'
+import FloorPlans from './components/FloorPlans';
 import Projects from './components/Projects';
 import Testimonails from './components/Testimonails';
+import LocationMap from './components/locationMap';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+// extra page
+import NotFound from './pages/NotFound';
 import TermsAndConditions from './pages/TermsAndConditions';
 import AboutUs from './pages/AboutUs';
+
+// dependencies of projects
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -71,6 +79,7 @@ const AppContent = () => {
           />
           <Route path="/Tnc" element={<TermsAndConditions />} />
           <Route path="/about-us" element={<AboutUs />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
 
