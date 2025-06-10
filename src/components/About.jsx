e9ed8c087a819e012e1398753a6b1b53d86fe6cb
CodeWithShubham-svg <hypersect000@gmail.com>
2025-06-10 16:08:32 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,7 +37,6 @@ const About = () => {
 
     <div className='mt-5'>
         <div className='flex gap-2'>
-   <p className='mb-2 text-[15px] md:font-3xl text-black text-start bg-blue-300 px-3 py-2 w-2/3 border-blue-600 border rounded-full font-semibold address sm:w-1/2'><b>📍</b> <span>Amroli, Surat, Gujarat</span> </p>
     <p className='mb-2 text-[15px] md:font-3xl text-white text-start bg-blue-300 px-3 py-2 w-2/3 border-blue-600 border rounded-full font-semibold contact sm:w-1/6 flex'><span><svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
