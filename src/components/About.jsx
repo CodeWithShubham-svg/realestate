429f93cca9494e74596caea5e8d33ee1849ed37d
CodeWithShubham-svg <hypersect000@gmail.com>
2025-06-10 16:11:23 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
         </div>
     </div>
 
-
+   <div className="mt-5">
     <p className='mb-8 mt-3 max-w-lg'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
     </div>
 
